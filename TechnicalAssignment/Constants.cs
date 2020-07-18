008d4e183d08033375d3465d3d573aa8c7ca46d4
YaremaVuyiv <Felix.Martin.Ivan@gmail.com>
2020-07-18 17:15:19 +0300

    Setup project. Add controller and action methods for upload. Add views for upload. Add constants

---
+++ TechnicalAssignment/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicalAssignment
+{
+    public static class Constants
+    {
+        public static List<string> AllowedFileExtensions = new List<string>
+        {
+            ".csv",
+            ".xml"
+        };
+    }
+}
---
+++ TechnicalAssignment/Controllers/TransactionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Mime;
+
+namespace TechnicalAssignment.Controllers
+{
+    [Route("Transaction")]
+    public class TransactionController : Controller
+    {
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("Index")]
+        public IActionResult Index(IFormFile uploadFile)
+        {
+            var fileExtension = Path.GetExtension(uploadFile.FileName);
+            if (!Constants.AllowedFileExtensions.Contains(fileExtension))
+            {
+                return View("Error");
+            }
+
+            return Ok();
+        }
+    }
+}
